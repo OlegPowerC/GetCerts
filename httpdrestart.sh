c787d4393cf49bb2e9cc177af4b9dc311ffc1e70
oleg@powerc.ru <OlegPowerC>
2023-05-18 18:05:24 +0300

    Initial

--- httpdrestart.sh
+++ httpdrestart.sh
@@ -2,8 +2,8 @@
 echo "stop httpd"
 service httpd stop
 echo "copy files"
-cp yourcert.cer /etc/httpd/conf/cert.crt
-cp yourkey.key /etc/httpd/conf/key.key
+cp cert.cer /etc/httpd/conf/cert.crt
+cp key.key /etc/httpd/conf/key.key
 echo "start httpd"
 service httpd start
 
